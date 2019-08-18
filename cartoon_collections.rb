98685a8629dc502aca0c212b00e7d070356e7ef4
Levi Borland <borland.levi@gmail.com>
2019-08-18 08:28:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,9 +34,9 @@ end
 
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include? do |ingredient
 end
 
 
