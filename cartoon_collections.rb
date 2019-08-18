cb7bd814d469f140e258b6c52085f0895e35d456
Levi Borland <borland.levi@gmail.com>
2019-08-18 08:09:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,13 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |dwarf, index|
-    puts "#{(index + 1)}. #{dwarf}"
-  end
+  array.each_with_index {|dwarf, index| puts "#{(index + 1)}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  return_array = []
+  i = 0 
+  while i < planeteer_calls.length 
+    
+  end
 end
 
 def long_planeteer_calls# code an argument here
@@ -16,3 +18,6 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
